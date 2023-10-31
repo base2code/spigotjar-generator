896fa0c23e6a26dbb3a83de0ab43c9dfec41d622
base2code <niklas.grenningloh@gmx.de>
2023-10-31 23:12:28 +0100

    Update variables

--- generate.sh
+++ generate.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 ARCH="aarch64" # x86_64, aarch64
-os="mac" # linux, mac
-mac_m1_workaround=true
+OS="mac" # linux, mac
+# There is no jdk 8 build for aarch64 on macos, so we need to use the x86_64 build (and run it with rosetta)
+MAC_M1_WORKAROUND=true
+
+MAVEN_GROUP_ID="org.spigotmc"
+MAVEN_ARTIFACT="spigot"
+MAVEN_REPO_ID="maven-releases"
+MAVEN_REPO_URL=""
 
 BUILDTOOLS_DOWNLOAD="https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
 
@@ -24,14 +30,14 @@ done
 
 for javaVersion in $JAVA_VERSIONS; do
   arch=$ARCH
-  if [ $mac_m1_workaround == "true" ] && [ "$javaVersion" == "8" ]; then
+  if [ $MAC_M1_WORKAROUND == "true" ] && [ "$javaVersion" == "8" ]; then
     arch="x64"
   fi
   # Get Release name
   prev=$(((javaVersion - 1)))
   next=$(((javaVersion + 1)))
   release=$(curl -X 'GET' \
-               "https://api.adoptium.net/v3/info/release_names?architecture=$arch&heap_size=normal&image_type=jdk&jvm_impl=hotspot&os=$os&page=0&page_size=10&project=jdk&release_type=ga&semver=false&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse&version=%28$prev%2C$next%5D" \
+               "https://api.adoptium.net/v3/info/release_names?architecture=$arch&heap_size=normal&image_type=jdk&jvm_impl=hotspot&os=$OS&page=0&page_size=10&project=jdk&release_type=ga&semver=false&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse&version=%28$prev%2C$next%5D" \
                -H 'accept: application/json' | jq -r '.releases[0]')
   if [ -z "$release" ]; then
     echo "Error: Release name for $javaVersion is empty." >&2
@@ -40,12 +46,12 @@ for javaVersion in $JAVA_VERSIONS; do
   echo "Release name for $javaVersion is $release"
 
   echo "Downloading java version $javaVersion for arch $arch"
-  filename=$(wget --server-response -q -O - "https://api.adoptium.net/v3/binary/latest/$javaVersion/ga/$os/$arch/jdk/hotspot/normal/eclipse?project=jdk" 2>&1 |
+  filename=$(wget --server-response -q -O - "https://api.adoptium.net/v3/binary/latest/$javaVersion/ga/$OS/$arch/jdk/hotspot/normal/eclipse?project=jdk" 2>&1 |
     grep "Content-Disposition:" | tail -1 | cut -d ';' -f2 | sed 's/filename=//g' | tr -d '\r')
   echo "Downloading $filename"
   if ! [ -f "$filename" ]; then
     echo "Download"
-    wget -O "$filename" "https://api.adoptium.net/v3/binary/latest/$javaVersion/ga/$os/$arch/jdk/hotspot/normal/eclipse?project=jdk"
+    wget -O "$filename" "https://api.adoptium.net/v3/binary/latest/$javaVersion/ga/$OS/$arch/jdk/hotspot/normal/eclipse?project=jdk"
     # Remove spaces from filename
     mv "$filename" $(echo "$filename" | tr -d ' ')
     filename=$(echo "$filename" | tr -d ' ')
@@ -64,31 +70,31 @@ for mapping in $(cat mapping.csv); do
   ver=$(echo "$mapping" | cut -d';' -f1)
   javaVersion=$(echo "$mapping" | cut -d';' -f2)
   javaBin=${JAVA_DIR[$javaVersion]}bin/java
-  if [ $os == "mac" ]; then
+  if [ $OS == "mac" ]; then
     javaBin=${JAVA_DIR[$javaVersion]}Contents/Home/bin/java
   fi
   chmod +x "$javaBin"
 
   javaBin=../$javaBin # Further down we cd into $ver
 
-  if [ $mac_m1_workaround == "true" ] && [ "$javaVersion" == "8" ]; then
+  if [ $MAC_M1_WORKAROUND == "true" ] && [ "$javaVersion" == "8" ]; then
       javaBin="arch -x86_64 $javaBin"
   fi
 
   mkdir -p "$ver"
   cp BuildTools.jar "$ver"
-  cd "$ver"
+  cd "$ver" || exit
 
   $javaBin -jar BuildTools.jar --output-dir "$ver" --rev "$ver"
   filepath="spigot-$ver.jar"
 
-  mvn deploy:deploy-file -DgroupId=org.spigotmc-test \
-      -DartifactId=spigot \
+  mvn deploy:deploy-file -DgroupId=$MAVEN_GROUP_ID \
+      -DartifactId=$MAVEN_ARTIFACT \
       -Dversion="$ver" \
       -Dpackaging=jar \
       -Dfile="$ver/$filepath" \
-      -DrepositoryId=lyrotopia-group \
-      -Durl=https://nexus.lyrotopia.net/repository/maven-releases
+      -DrepositoryId=$MAVEN_REPO_ID \
+      -Durl=$MAVEN_REPO_URL
 
   cd ..
 done
